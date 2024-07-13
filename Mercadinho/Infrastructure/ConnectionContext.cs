2fce125ffe1b1bc3cf0e72e12116b381d1b426a6
GhostNick <nikvieira07@gmail.com>
2024-07-13 03:23:10 -0300

    Adicionar arquivos de projeto.

---
+++ Mercadinho/Infrastructure/ConnectionContext.cs
@@ -0,0 +1,11 @@
+﻿using Mercadinho.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mercadinho.Infrastructure
+{
+    public class ConnectionContext : DbContext
+    {
+        public ConnectionContext(DbContextOptions<ConnectionContext> options) : base(options){}
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Mercadinho/Model/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mercadinho.Model
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price  { get; set; }
+        public int Amount { get; set; }
+    }
+}
